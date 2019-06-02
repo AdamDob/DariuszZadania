0edc4318c1466eac55678eba3a67b1485a5e5d6a
Adam Dobrzyckiemail <adam2dobrzycki@gmail.com>
2019-06-02 12:37:47 +0200

    Zadanie 2.2

---
+++ src/Zad22.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zad21 {
+
+    public static void main(String[] args) {
+        boolean dzialaj = true;
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        int ruchy = 0;
+        
+        int a = rand.nextInt(int n);
+        System.out.println(a);
+        while (dzialaj){
+
+            int zgaduj = sc.nextInt();
+            if(zgaduj == a){
+                System.out.println("Brawo odgales");
+                ruchy = ruchy +1;
+                System.out.println("Wykonales " + ruchy + " ruchy");
+                dzialaj = false;
+            }
+            else if(zgaduj > a){
+                System.out.println("Ups Twoja liczba jest zby wielka");
+                ruchy = ruchy + 1;
+            }
+            else if(zgaduj < a){
+                System.out.println("Ups Twoja liczba jest zbyt mala");
+                ruchy = ruchy +1;
+            }
+        }
+    }
+}
