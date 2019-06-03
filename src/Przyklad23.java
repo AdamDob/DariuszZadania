2adfb79337a58cf80fa7bdbc272fdd33c1170da4
Adam Dobrzyckiemail <adam2dobrzycki@gmail.com>
2019-06-03 20:35:07 +0200

    Zad2.6

--- src/Przyklad23.java
+++ src/Przyklad23.java
@@ -1,18 +1,40 @@
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Przyklad23 {
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         Random rand = new Random();
-        int tab [] = new int[10];
+        Scanner in = new Scanner(System.in);
 
+        int tab[] = new int[10];
 
-       for(int i =0; i<tab.length; i++){
-           tab[i] = rand.nextInt(10);
-           System.out.println ("Liczba losowa " +tab[i] +  "  " + " indekst =  " + i );
+        for(int i=0; i<tab.length;i++){
+            tab[i] = rand.nextInt(100);
+            System.out.print(tab[i] + " ");
+        }
 
+        System.out.println("Podaj liczbe ktorej szukasz");
+        int value = in.nextInt();
+        Przyklad23 przy = new Przyklad23();
+
+        if(przy.contains(tab,value)){
+            System.out.println(String.format("Liczba %d znaleziona! ", value));
+        }
+        else{
+            System.out.println("Przykro nam liczba nie zbnaleziona ");
         }
     }
+
+    public boolean contains(int[] tab, int v){
+        boolean result = false;
+        for(int i : tab){
+            if(i == v){
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
